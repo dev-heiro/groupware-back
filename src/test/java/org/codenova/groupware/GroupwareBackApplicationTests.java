4880b4e7760f0587f772c009b0766ed7094c4e0f
heiro <dev.heiro@gmail.com>
2025-04-28 10:16:23 +0900

    chore : 프로젝트 초기 설정 완료

---
+++ src/test/java/org/codenova/groupware/GroupwareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codenova.groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
